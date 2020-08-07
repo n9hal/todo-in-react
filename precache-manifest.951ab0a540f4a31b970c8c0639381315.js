8b0ca0b30c9bcf11d02aa06261454172e6547cb7
n9hal <the.n9hal@gmail.com>
2020-08-07 11:46:41 +0530

    Updates

---
+++ precache-manifest.951ab0a540f4a31b970c8c0639381315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876dcedda4bb1e77f4521fce4bd1490e",
+    "url": "/n9hal/todo-in-react.git/index.html"
+  },
+  {
+    "revision": "b08d6cdf10c9e9ceaf1f",
+    "url": "/n9hal/todo-in-react.git/static/css/main.0fa240fc.chunk.css"
+  },
+  {
+    "revision": "ae417a89f22d29706585",
+    "url": "/n9hal/todo-in-react.git/static/js/2.882f0e08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/n9hal/todo-in-react.git/static/js/2.882f0e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08d6cdf10c9e9ceaf1f",
+    "url": "/n9hal/todo-in-react.git/static/js/main.66a2fefd.chunk.js"
+  },
+  {
+    "revision": "f2671541ecbbd7e7013f",
+    "url": "/n9hal/todo-in-react.git/static/js/runtime-main.2a7e90d8.js"
+  }
+]);
